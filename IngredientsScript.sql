51b65726df90cbb2e189f80e291516258b6b6387
Kyle Pottinger <kylep@bbd.co.za>
2023-02-20 18:20:10 +0200

    Removed redundant foreign keys and fields

--- IngredientsScript.sql
+++ IngredientsScript.sql
@@ -1,7 +1,5 @@
 CREATE TABLE Ingredients(
 	IngredientID int IDENTITY(1,1) NOT NULL,
 	IngredientName varchar (120) NOT NULL,
-	QuantityID int NOT NULL,
 	CONSTRAINT PK_Ingredient PRIMARY KEY (IngredientID ASC),
-	CONSTRAINT FK_Quantity FOREIGN KEY (QuantityID) REFERENCES Quantity (QuantityID)
 );
--- QuantityScript.sql
+++ QuantityScript.sql
@@ -1,7 +1,5 @@
 CREATE TABLE Quantity(
 	QuantityID int IDENTITY(1,1) NOT NULL,
-	IngredientName varchar (120) NOT NULL,
-	IngredientID int NOT NULL,
 	MeasurementTypeID int NOT NULL,
 	QuantityAmount int NOT NULL,
 	CONSTRAINT PK_Quantity PRIMARY KEY (QuantityID ASC),
