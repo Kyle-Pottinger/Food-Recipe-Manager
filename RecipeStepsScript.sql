4a152390b5d2fa5ac54cdadb0c478f9d6af0282c
Line Dorfling <line@bbd.co.za>
2023-02-20 19:05:20 +0200

    added recipe id to recipe steps table

--- RecipeStepsScript.sql
+++ RecipeStepsScript.sql
@@ -3,8 +3,10 @@ CREATE TABLE RecipeSteps(
     Instruction varchar(8000) NULL,
     IngredientID int NOT NULL,
     QuantityID int NOT NULL,
+    RecipeID int NOT NULL,
 	CONSTRAINT PK_RecipeSteps PRIMARY KEY (RecipeStepID ASC),
 	CONSTRAINT FK_IngredientID FOREIGN KEY (IngredientID) REFERENCES Ingredients (IngredientID),
 	CONSTRAINT FK_QuantityID FOREIGN KEY (QuantityID) REFERENCES Quantity (QuantityID)
+	CONSTRAINT FK_RecipeID FOREIGN KEY (RecipeID) REFERENCES Recipes (RecipeID)
 );
 GO
