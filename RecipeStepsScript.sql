ac849a434425378118a94edbda1c039978fdb45e
Line Dorfling <line@bbd.co.za>
2023-02-20 17:30:33 +0200

    added categories and recipesteps table creation

---
+++ RecipeStepsScript.sql
@@ -0,0 +1,10 @@
+CREATE TABLE RecipeSteps(
+    RecipeStepID int IDENTITY(1,1) NOT NULL,
+    Instruction varchar(8000) NULL,
+    IngredientID int NOT NULL,
+    QuantityID int NOT NULL,
+	CONSTRAINT PK_RecipeSteps PRIMARY KEY (RecipeStepID ASC),
+	CONSTRAINT FK_IngredientID FOREIGN KEY (IngredientID) REFERENCES Ingredients (IngredientID),
+	CONSTRAINT FK_QuantityID FOREIGN KEY (QuantityID) REFERENCES Quantities (QuantityID)
+);
+GO
